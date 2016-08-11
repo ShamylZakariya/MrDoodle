8d2ddb8faa57182856112178b9d3be5b55f93aee
Shamyl Zakariya <shamyl@gmail.com>
2016-08-11 09:01:18 -0700

    Automatically update local IP address in builds for testing

--- android/MrDoodle/app/src/main/java/org/zakariya/mrdoodle/net/SyncServerConfiguration.java
+++ android/MrDoodle/app/src/main/java/org/zakariya/mrdoodle/net/SyncServerConfiguration.java
@@ -1,5 +1,7 @@
 package org.zakariya.mrdoodle.net;
 
+import org.zakariya.mrdoodle.BuildConfig;
+
 /**
  * Created by shamyl on 8/8/16.
  */
@@ -18,7 +20,7 @@ public class SyncServerConfiguration {
 	String getSyncServerHost() {
 		return isEmulator
 				? "10.0.2.2:4567"
-				: "10.0.1.51:4567";
+				: BuildConfig.LOCAL_IP + ":4567";
 	}
 
 	String getSyncServerConnectionUrl() {
